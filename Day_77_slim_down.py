8868504d72b5c925e8841cca1759a65367317868
kelvin <kelvin.morcillo@gmail.com>
2025-05-04 07:54:39 -0300

    fix: support Railway deployment

--- Day_77_slim_down.py
+++ Day_77_slim_down.py
@@ -62,5 +62,6 @@ def redirect_hello():
 def redirect_bye():
     return redirect('/you-say-bye')
 # --- Standard Run Settings ---
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
